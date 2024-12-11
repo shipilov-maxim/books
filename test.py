c588c780b696cb8aaa7435ad719aa4fc3b03c3b6
Maxim Shipilov <skam3840@gmail.com>
2024-12-11 13:57:08 +0400

    fix test

--- test.py
+++ test.py
@@ -1,8 +1,10 @@
 import pytest
+
+from book import Book
 from book_manager import BookManager
 
 
-@pytest.fixture
+@pytest.fixture()
 def setup_book_manager():
     books = [
         {
@@ -23,28 +25,33 @@ def setup_book_manager():
     return BookManager(books)
 
 
-def test_add_book(setup_book_manager):
+@pytest.fixture()
+def reset_book_ids():
+    Book.existing_ids = set()
+
+
+def test_add_book(reset_book_ids, setup_book_manager):
     setup_book_manager.add_book("Test Book 3", "author 3", "2023")
     assert len(setup_book_manager.books) == 3
     assert setup_book_manager.books[2].title == "Test Book 3"
 
 
-def test_view_book(setup_book_manager):
+def test_view_book(reset_book_ids, setup_book_manager):
     setup_book_manager.view_book()
     assert len(setup_book_manager.books) == 2
 
 
-def test_status_book(setup_book_manager):
-    setup_book_manager.status_book(6)
+def test_status_book(reset_book_ids, setup_book_manager):
+    setup_book_manager.status_book(1)
     assert setup_book_manager.books[0].status == "Выдана"
-    setup_book_manager.status_book(6)
+    setup_book_manager.status_book(1)
     assert setup_book_manager.books[0].status == "В наличии"
 
 
-def test_delete_book(setup_book_manager):
-    setup_book_manager.delete_book(8)
+def test_delete_book(reset_book_ids, setup_book_manager):
+    setup_book_manager.delete_book(1)
     assert len(setup_book_manager.books) == 1
 
 
-def test_search_book(setup_book_manager):
-    assert setup_book_manager.search_book("Book 1")[0].title == "Test Book 1"
+def test_search_book(reset_book_ids, setup_book_manager):
+    assert setup_book_manager.search_book("Book 2")[0].title == "Test Book 2"
